ad442a421e7c7a7332e4ac6b4d8ed6c220f2b85e
ludovic-cyber <82147321+ludovic-cyber@users.noreply.github.com>
2021-05-28 20:43:13 +0200

    To test with process_pred

---
+++ test_api.py
@@ -0,0 +1,14 @@
+import datetime
+import dataGeter
+import graph
+import process
+
+date = datetime.date(2021, 5, 15)
+sensor_code = 'K268081001'
+
+
+measures_list = dataGeter.GetMeasures(date, 10, sensor_code)
+prediction = []
+prediction = process.predict_arma(measures_list[:72],24)
+
+graph.ShowMeasures(measures_list, prediction)
